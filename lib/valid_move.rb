0022bd66cc24feac2f81139ac6cfd2c8a08fd74b
Zachary Durr <zdurr22@gmail.com>
2020-09-14 21:10:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
     true
   elsif index.between?(0, 8) == false
     false
-  elsif index.between?(0, 8) == true && position_taken?(board, index) == true
+  elsif (index.between?(0, 8) && position_taken?(board, index)) == true
     false
   else
     false
