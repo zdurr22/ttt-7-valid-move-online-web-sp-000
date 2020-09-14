b5de8e93530926f9f650c23d9107440c2fb300aa
Zachary Durr <zdurr22@gmail.com>
2020-09-14 21:17:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -20,7 +20,7 @@ def position_taken?(board, index)
     false
   elsif board[index] == nil
     false
-  elsif board[index] == "X" || board[4] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     true
   end
 end
