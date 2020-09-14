e275fda9fa3350d301c901aae335e5685587e794
Zachary Durr <zdurr22@gmail.com>
2020-09-14 21:04:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,7 @@ def valid_move?(board, index)
     false
   elsif position_taken?(board, index) == false || nil
     false
-  elsif position_taken?(board, index) == true
+  elsif position_taken? == true
     false
   else
     true
